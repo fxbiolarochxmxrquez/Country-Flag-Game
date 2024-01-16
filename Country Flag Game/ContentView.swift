6aaa895d14b0087ae13bf62b9ad566ab2820cd49
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-01-16 16:29:31 -0600

    Initial Commit

---
+++ Country Flag Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Country Flag Game
+//
+//  Created by Fabiola Rocha Marquez on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by Fabiola Rocha Marquez on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
