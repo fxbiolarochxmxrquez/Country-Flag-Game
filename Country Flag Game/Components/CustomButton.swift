d4fbf3d24abbf2ccdce1a4cc2016c187bb108c87
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-01-16 17:56:46 -0600

    added custom button, progress bar, and answer row

---
+++ Country Flag Game/Components/CustomButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButton.swift
+//  Country Flag Game
+//
+//  Created by Fabiola Rocha Marquez on 1/16/24.
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButton()
+}
