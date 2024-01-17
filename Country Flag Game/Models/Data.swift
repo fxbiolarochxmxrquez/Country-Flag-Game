551bef6189f4c9d3c858eda6121b66e09dad8191
Fabiola Rocha Marquez <frochamarquez@bsd220.org>
2024-01-16 18:56:43 -0600

    created data and game manager

---
+++ Country Flag Game/Models/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Country Flag Game
+//
+//  Created by Fabiola Rocha Marquez on 1/16/24.
+//
+
+import Foundation
---
+++ Country Flag Game/Models/GameManager.swift
@@ -0,0 +1,8 @@
+//
+//  GameManager.swift
+//  Country Flag Game
+//
+//  Created by Fabiola Rocha Marquez on 1/16/24.
+//
+
+import Foundation
